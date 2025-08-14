7addd9096e155b88f8656c9c6b32c745dcb26f9f
ShakiraBR <dev@shakirabr.com>
2025-08-14 18:36:06 -0300

    fix: ajustar configuração para domínio sem www (shakirabr.com) conforme Google Search Console verificado

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://www.shakirabr.com'
+  const baseUrl = 'https://shakirabr.com'
 
   return {
     rules: [
